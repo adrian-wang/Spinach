1d5ca28ca92080656e4c99573e6ad32d84e91419
Daoyuan Wang <daoyuan.wang@intel.com>
2016-11-01 04:21:53 -0700

    use SINDEX instead of SPINACH INDEX, and fix index existing when CREATE SINDEX IF NOT EXISTS

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/spinach/DataSourceMetaSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/spinach/DataSourceMetaSuite.scala
@@ -145,13 +145,20 @@ class DataSourceMetaSuite extends SharedSQLContext with BeforeAndAfter {
 
     assert(spinachMeta.schema === df.schema)
 
-    sql("create index index1 on spnt1 (a) using btree")
-    sql("create index index2 on spnt1 (a asc)") // dup as index1, still creating
-    sql("create index index3 on spnt1 (a desc)")
-    sql("create index if not exists index3 on spnt1 (a desc)") // not creating
-    sql("drop index index2") // dropping
-    sql("drop index if exists index5") // not dropping
-    sql("drop index if exists index2") // not dropping
+    sql("create sindex index1 on spnt1 (a)")
+    assert(DataSourceMeta.initialize(path, new Configuration()).fileHeader.indexCount === 1)
+    sql("create sindex index2 on spnt1 (a asc)") // dup as index1, still creating
+    assert(DataSourceMeta.initialize(path, new Configuration()).fileHeader.indexCount === 2)
+    sql("create sindex index3 on spnt1 (a desc)")
+    assert(DataSourceMeta.initialize(path, new Configuration()).fileHeader.indexCount === 3)
+    sql("create sindex if not exists index3 on spnt1 (a desc)") // not creating
+    assert(DataSourceMeta.initialize(path, new Configuration()).fileHeader.indexCount === 3)
+    sql("drop sindex index2 on spnt1") // dropping
+    assert(DataSourceMeta.initialize(path, new Configuration()).fileHeader.indexCount === 2)
+    sql("drop sindex if exists index5 on spnt1") // not dropping
+    assert(DataSourceMeta.initialize(path, new Configuration()).fileHeader.indexCount === 2)
+    sql("drop sindex if exists index2 on spnt1") // not dropping
+    assert(DataSourceMeta.initialize(path, new Configuration()).fileHeader.indexCount === 2)
 
     val spinachMeta2 = DataSourceMeta.initialize(path, new Configuration())
     val fileHeader2 = spinachMeta2.fileHeader
